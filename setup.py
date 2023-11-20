02ea4d7105dab55fd630364a2cf3841c4f9eb09c
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2023-11-20 17:08:35 -0500

    un exemple pour montrer comment publier un package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="addition de deux nombres",
+    version="0.0.1",
+    author="Baouly Nelson",
+    author_email="baoulynelson@egmail.com",
+    description="Un petit exemple de package",
+    install_requires=[
+    ],
+    url="https://github.com/BaoulyNelson/exemplePackage.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
---
+++ src/exemple_package BaoulyNelson/exemple.py
@@ -0,0 +1,5 @@
+def addition_des_nombres(nombre1, nombre2):
+    resultat = nombre1 + nombre2
+    print(resultat)
+    
+addition_des_nombres(30, 78)
